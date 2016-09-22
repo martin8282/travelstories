b4bdfe7431c00e93d9cf346441f33f0e4cd6c596
Dmitri Shahniuk <dimon.shahniuk@gmail.com>
2016-09-22 16:44:43 +0300

    Updated animation.

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,44 +1,48 @@
 var app = {
   current_frame: null,
-  
-  loadFrame: function(frameName, complete) {
+
+  loadFrame: function (frameName, complete) {
     $.ajax({
       url: 'frames/' + frameName + '.html',
       method: 'get',
       success: complete
     })
   },
-  
-  navigate: function(frameName) {
+
+  navigate: function (frameName) {
     var body = $('body');
-    
-    var slide = function(oldFrame, newFrame, onComplete) {
-      oldFrame.css({ 'left': 0, 'z-index': 0 }).removeClass('current');
-      
-      newFrame
-        .addClass('moving')
-        .height($(window).height())
-        .removeClass('hidden');
-      
-      var animationComplete = function() {
+
+    var slide = function (oldFrame, newFrame, onComplete) {
+      oldFrame.css({'left': 0, 'z-index': 0}).removeClass('current');
+
+      var winHeight = $(window).height();
+      var scrollTop = $(window).scrollTop();
+
+      newFrame.addClass('moving').css({top: scrollTop, height: winHeight}).removeClass('hidden');
+
+      var animationComplete = function () {
+        oldFrame.attr('scroll', scrollTop);
+        alert(scrollTop);
         onComplete(oldFrame, newFrame);
       };
-  
-      newFrame.animate({ left: '0' }, { duration: 300, complete: animationComplete });
+
+      newFrame.animate({left: '0'}, {duration: 300, complete: animationComplete});
     };
-    
-    var complete = function(oldFrame, newFrame) {
+
+    var complete = function (oldFrame, newFrame) {
       if (oldFrame) oldFrame.addClass('hidden').removeAttr('style');
+
       newFrame.attr('id', frameName).removeClass('moving').addClass('current').removeAttr('style');
-  
+      if (newFrame.attr('scroll')) $(window).scrollTop(newFrame.attr('scroll'));
+
       body.css('overflow', 'auto');
     };
-  
-    var process = function(newFrame) {
+
+    var process = function (newFrame) {
       body.css('overflow', 'hidden');
-      
+
       var oldFrame = $('.frame.current');
-      
+
       if (oldFrame.length) {
         slide(oldFrame, newFrame, complete);
       }
@@ -46,13 +50,13 @@ var app = {
         complete(null, newFrame);
       }
     };
-    
-    var onLoadFrame = function(response) {
+
+    var onLoadFrame = function (response) {
       var newFrame = $(response);
       body.append(newFrame);
       process(newFrame);
     };
-  
+
     var newFrame = $('#' + frameName);
     if (newFrame.length) {
       process(newFrame)
